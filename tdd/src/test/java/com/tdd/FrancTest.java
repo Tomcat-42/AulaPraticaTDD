28a8012ba8353d6d014fbac57c8397c8c6ab67cc
Pablo <pablohuggem@gmail.com>
2022-08-10 19:02:40 -0300

    Adiciona testes da classe Franc

---
+++ tdd/src/test/java/com/tdd/FrancTest.java
@@ -0,0 +1,24 @@
+package com.tdd;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+
+import com.tdd.Franc;
+
+public class FrancTest {
+  @Test
+  public void testMultiplication() {
+    Franc five = new Franc(5);
+    assertTrue(new Franc(10).equals(five.times(2)));
+    assertTrue(new Franc(15).equals(five.times(3)));
+  }
+
+  @Test
+  public void testEquality() {
+    assertTrue(new Franc(5).equals(new Franc(5)));
+    assertFalse(new Franc(5).equals(new Franc(6)));
+  }
+}
